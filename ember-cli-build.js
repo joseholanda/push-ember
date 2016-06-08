da71f2ef4187b664df39061f85f62d5da36a44b9
joseholanda <jose.holanda.neto@gmail.com>
2016-06-08 01:13:34 -0300

    primeiro commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,23 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/AdminLTE/bootstrap/css/bootstrap.min.css');
+  app.import('bower_components/AdminLTE/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+  { srcDir: 'AdminLTE/bootstrap',destDir: 'fonts' });
+  app.import('bower_components/components-font-awesome/css/font-awesome.min.css');
+  app.import('bower_components/components-font-awesome/fonts/fontawesome-webfont.woff2',
+  { srcDir: 'components-font-awesome',destDir: 'fonts' });
+  app.import('bower_components/Ionicons/css/ionicons.min.css');
+  app.import('bower_components/Ionicons/fonts/ionicons.woff', {destDir: 'fonts'});
+  app.import('bower_components/AdminLTE/dist/css/AdminLTE.min.css', { destDir: 'dist' });
+  app.import('bower_components/AdminLTE/dist/css/skins/skin-blue.min.css');
+
+
+
+
+  app.import('bower_components/AdminLTE/bootstrap/js/bootstrap.min.js');
+  app.import('bower_components/AdminLTE/dist/js/app.min.js');
+
 
   return app.toTree();
 };
